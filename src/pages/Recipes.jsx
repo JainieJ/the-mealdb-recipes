576c5b60fcc8ffece445f9135b6d6b8ad3e83fbe
Janie <evgeniyaberseneva89@gmail.com>
2019-05-28 15:11:23 +0300

    handling of no-result error

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -11,7 +11,8 @@ class Recipes extends Component {
     search: "",
     ingredients: [],
     meals: [],
-    recipeRequested: false
+    recipeRequested: false,
+    error: ""
   };
 
   async componentDidMount() {
@@ -43,13 +44,25 @@ class Recipes extends Component {
         }/search.php?s=${search}`
       );
       const recipeJson = await recipeResponse.json();
-      this.setState({ meals: recipeJson.meals, recipeRequested: true });
+      if (!recipeJson.meals) {
+        this.setState({
+          error: "Search returned no results. Please, try again",
+          search: ""
+        });
+      } else {
+        this.setState({
+          meals: recipeJson.meals,
+          recipeRequested: true,
+          error: "",
+          search: ""
+        });
+      }
     } catch (err) {
       console.log(err);
     }
   }
   render() {
-    const { meals, ingredients, search, recipeRequested } = this.state;
+    const { meals, ingredients, search, recipeRequested, error } = this.state;
     return (
       <div className="container">
         <Search
@@ -57,29 +70,35 @@ class Recipes extends Component {
           handleChange={this.handleChange}
           handleSubmit={this.handleSubmit}
         />
-        {/* row for recipes by name or ingredients */}
+        {/* row for recipes by name or ingredients, or for showing search error*/}
         <div className="row">
-          {recipeRequested
-            ? meals.map(meal => (
-                <ListItem
-                  key={meal.idMeal}
-                  img={meal.strMealThumb}
-                  title={meal.strMeal}
-                  linkUrl={`/recipes/${meal.idMeal}`}
-                  linkText="details"
-                  styleClass="col-sm-6 col-md-4"
-                />
-              ))
-            : ingredients.map(ingredient => (
-                <ListItem
-                  key={ingredient.idCategory}
-                  img={ingredient.strCategoryThumb}
-                  title={ingredient.strCategory}
-                  linkUrl={`/recipes/main_ingredient/${ingredient.strCategory}`}
-                  linkText="see more"
-                  styleClass="col-md-6 col-lg-4"
-                />
-              ))}
+          {error ? (
+            <div className="col-10 mx-auto text-center mt-5">
+              <h3 className="text-muted">{error}</h3>
+            </div>
+          ) : recipeRequested ? (
+            meals.map(meal => (
+              <ListItem
+                key={meal.idMeal}
+                img={meal.strMealThumb}
+                title={meal.strMeal}
+                linkUrl={`/recipes/${meal.idMeal}`}
+                linkText="details"
+                styleClass="col-sm-6 col-md-4"
+              />
+            ))
+          ) : (
+            ingredients.map(ingredient => (
+              <ListItem
+                key={ingredient.idCategory}
+                img={ingredient.strCategoryThumb}
+                title={ingredient.strCategory}
+                linkUrl={`/recipes/main_ingredient/${ingredient.strCategory}`}
+                linkText="see more"
+                styleClass="col-md-6 col-lg-4"
+              />
+            ))
+          )}
         </div>
       </div>
     );
